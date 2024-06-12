baf6c828248b4c32cb913edaee564c1aa47fa0e6
Marcelo Meirelles <meirellesm@hotmail.com>
2024-06-11 22:30:06 -0300

    change: node version

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 
 npm install
 yarn run build
-npm run typeorm migration:run
+npm run typeorm migration:run -- -d dist/data-source
--- src/migrations/1718154613409-InitialMigration.ts
+++ src/migrations/1718154613409-InitialMigration.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Initial1718131078111 implements MigrationInterface {
-    name = 'Initial1718131078111'
+export class InitialMigration1718154613409 implements MigrationInterface {
+    name = 'InitialMigration1718154613409'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "proteins" ("id" SERIAL NOT NULL, "imageInactive" character varying NOT NULL, "imageActive" character varying NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, "price" integer NOT NULL, CONSTRAINT "UQ_398b8a05b8dd76679d0128a1d22" UNIQUE ("name"), CONSTRAINT "PK_32956cc233035917f693d0412cf" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "orders" ("id" SERIAL NOT NULL, "description" character varying NOT NULL, "image" character varying NOT NULL, "brothId" integer, "proteinId" integer, CONSTRAINT "PK_710e2d4957aa5878dfe94e4ac2f" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "orders" ("id" character varying NOT NULL, "description" character varying NOT NULL, "image" character varying NOT NULL, "brothId" integer, "proteinId" integer, CONSTRAINT "PK_710e2d4957aa5878dfe94e4ac2f" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "broths" ("id" SERIAL NOT NULL, "imageInactive" character varying NOT NULL, "imageActive" character varying NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, "price" integer NOT NULL, CONSTRAINT "UQ_ac679fd0ba29ba25d153332f2fb" UNIQUE ("name"), CONSTRAINT "PK_f9ccad7cdda83e46906104a9228" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "orders" ADD CONSTRAINT "FK_d97bd962b7dbe2cf1a9eb7fb0db" FOREIGN KEY ("brothId") REFERENCES "broths"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "orders" ADD CONSTRAINT "FK_ddc193360a6984d832b437eb49b" FOREIGN KEY ("proteinId") REFERENCES "proteins"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
